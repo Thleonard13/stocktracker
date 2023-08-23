b5b5c8c55e7307736f5472f03eebc52c7fa4a894
Thomas Leonard <haydentmusic@gmail.com>
2023-08-23 12:23:24 -0700

    .env file added

--- src/Utilities/FetchStocks.js
+++ src/Utilities/FetchStocks.js
@@ -1,24 +1,23 @@
 // function for fetching stock information from API
 const fetchStockInfo = async (symbol) => {
     const url = `https://alpha-vantage.p.rapidapi.com/query?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=compact&datatype=json`;
-const options = {
-	method: 'GET',
-	headers: {
-        // API KEY REMOVED FOR GITHUB
-		'X-RapidAPI-Key': '',
-		'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
+			'X-RapidAPI-Host': 'alpha-vantage.p.rapidapi.com'
+		}
+	};
+
+	try {
+		const response = await fetch(url, options);
+		const result = await response.json();
+		// console.log('STOCK FETCHED: ',result);
+		return result
+
+	} catch (error) {
+		console.error(error);
 	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	// console.log('STOCK FETCHED: ',result);
-    return result
-
-} catch (error) {
-	console.error(error);
-}
 }
 
 export default fetchStockInfo;
