b7fc1648aff36beb20eb7c8d01c20cf7d5d72db5
Thomas Leonard <haydentmusic@gmail.com>
2023-08-24 10:42:59 -0700

    updated mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -13,10 +13,10 @@ const MobileMenu = (props) => {
   return (
     <section id="mobile-menu" className={mobileMenuClasses}>
       <nav>
-        <li><a href='/#hero-section' onClick={handleLinkClick}>Home</a></li>
-        <li><a href='/#market-section' onClick={handleLinkClick}>Market</a></li>
-        <li><a href='/#about-us-section' onClick={handleLinkClick}>Features</a></li>
-        <li><a href='/#join-us-section' onClick={handleLinkClick}>Join Us</a></li>
+        <a href='/#hero-section' onClick={handleLinkClick}><li>Home</li></a>
+        <a href='/#market-section' onClick={handleLinkClick}><li>Market</li></a>
+        <a href='/#about-us-section' onClick={handleLinkClick}><li>Features</li></a>
+        <a href='/#join-us-section' onClick={handleLinkClick}><li>Join Us</li></a>
       </nav>
     </section>
   );
